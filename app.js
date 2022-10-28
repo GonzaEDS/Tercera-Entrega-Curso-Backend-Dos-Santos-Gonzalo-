e1986011021d9732e3ac760009b42295b6eee662
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2022-10-28 02:53:15 -0300

    fixing form

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-// const router = require('./src/routes/index')
-
-// app.use(express.json())
-// app.use('/api', router)
-// app.use(express.static(__dirname + '/public'))
-// module.exports = app
-
-//
-
 require('dotenv').config()
 
 const express = require('express')
 const app = express()
 
-// const logger = require('morgan')
+app.use(express.static(__dirname + '/public'))
 
 const router = require('./src/routes/index')
-// const errorHandler = require('./src/middlewares/errorHandler')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', async (_req, res) => {
-  res.status(200).json({
-    env: process.env.ENVIROMENT || undefined,
-    port: process.env.PORT || 8000
-  })
-})
-app.use('/api', router)
-// app.use(errorHandler)
-// app.use(logger('dev'))
 
+app.use('/api', router)
 module.exports = app
