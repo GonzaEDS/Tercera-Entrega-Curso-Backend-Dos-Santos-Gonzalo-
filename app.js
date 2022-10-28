f8c3dd979415964a66ac21f76e213e9a967a79c3
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2022-10-28 01:28:39 -0300

    form

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const router = require('./src/routes/index')
+
+// app.use(express.json())
+// app.use('/api', router)
+// app.use(express.static(__dirname + '/public'))
+// module.exports = app
+
+//
+
 require('dotenv').config()
-const router = require('./src/routes/index')
+
 const express = require('express')
 const app = express()
 
+// const logger = require('morgan')
+
+const router = require('./src/routes/index')
+// const errorHandler = require('./src/middlewares/errorHandler')
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', async (_req, res) => {
+  res.status(200).json({
+    env: process.env.ENVIROMENT || undefined,
+    port: process.env.PORT || 8000
+  })
+})
 app.use('/api', router)
-app.use(express.static(__dirname + '/public'))
+// app.use(errorHandler)
+// app.use(logger('dev'))
+
 module.exports = app
